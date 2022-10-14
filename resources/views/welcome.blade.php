0bfb52d18e29cdf1a5234cce3c496f85d18b4889
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-10-14 17:46:54 +0200

    Inizio Challenge

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #EE4437;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                color: #f69b57;
+                font-size: 84px;
+                text-transform: uppercase;
+
+            }
+            .text-cs {
+                background-color: #f43340;
+                color: black;
+                font-size: 30px;
+                text-transform: uppercase;
+            }
+
+            .links > a {
+                color: blanchedalmond;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div>
+                    <h2 class="title m-b-md">Gym Future</h2>
+                    <p class="text-cs"><span>&#x1F3CB;&#xFE0F;</span> Cooming Soon <span>&#x23F3;</span></p>
+                </div>
+
+                <div class="links">
+                    <a href="#">Chi Siamo</a>
+                    <a href="#">News</a>
+                    <a href="#">Contatti</a>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
